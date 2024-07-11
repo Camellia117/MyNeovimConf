887e11026ca3566e926700c2ab9e0a79cf0ec84e
Camellia117 <embrace_death@163.com>
2024-07-11 21:34:10 +0800

    close the notify & <Tab> to select
    add Waketime plugin & some new conf

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 require("camellia.core")
 require("camellia.lazy")
+--
+
+-- require('neovim-lua.core/lazy')
+-- require('neovim-lua.core/autocmds')
+-- require('neovim-lua.core/keymaps')
+-- require('neovim-lua.core/colors')
+-- require('neovim-lua.core/statusline')
+-- require('neovim-lua.core/options')
+-- require('neovim-lua.lsp/lspconfig')
+-- require('neovim-lua.plugins/nvim-tree')
+-- require('neovim-lua.plugins/indent-blankline')
+-- require('neovim-lua.plugins/nvim-cmp')
+-- require('neovim-lua.plugins/nvim-treesitter')
+-- require('neovim-lua.plugins/alpha-nvim')
